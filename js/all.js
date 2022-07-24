6aeda06fc53ae51d4145e40e98b63141e286ccee
Allen Tsai <81896806+Allen-9527@users.noreply.github.com>
2022-07-24 17:39:18 +0800

    Update all.js
    修改JS程式寫法，著重在TAB切換狀態

--- js/all.js
+++ js/all.js
@@ -2,179 +2,121 @@ const list = document.querySelector(".list");
 const txtInput = document.querySelector(".txtInput");
 const btnAdd = document.querySelector(".btn_add");
 const listNum = document.querySelector(".list_footer p");
-const listCleanFinished = document.querySelector(".list_footer a");
+const cleanDone = document.querySelector(".list_footer a");
 
 //全部資料(預設資料，最後可以註解掉)
-let data =[
-    {
-        content:"事項一",
-        checked:""        
-    },
-    {
-        content:"事項二",
-        checked:""
-    },
-    {
-        content:"事項三",
-        checked:""
+let data =[];
+
+//1.新增事項功能
+btnAdd.addEventListener('click',addTodo);
+function addTodo(){
+    let obj = {
+        txt:txtInput.value,
+        id:new Date().getTime(),
+        checked:''
     }
-]
+    if(obj.txt !=''){
+        data.unshift(obj);
 
-//清除已完成項目用的空資料
-// let CleanData = [];
+    }
+    else{
+        alert("請輸入代辦事項");
+    }
+    //renderData();
+    updateList();
+}
 
-//1.初始化全部資料
-function renderData(){
+//2.初始化全部資料
+function renderData(dataObj){
     let str = '';
-    data.forEach(function(item,index){
-        str += `<li>
+    dataObj.forEach(function(item,index){
+        str += `<li data-id="${item.id}">
             <label class="checkbox" for="">
-            <input type="checkbox" id="checkbox" data-num="${index}" ${item.checked} />
-            <span>${item.content}</span>
+                <input type="checkbox"  ${item.checked} />
+                <span>${item.txt}</span>
             </label>
-            <a href="#" class="delete" data-num="${index}"></a></li>`
+            <a href="#" class="delete" ></a>
+            </li>`;
     })
     //console.log(str);
     list.innerHTML = str;
-    listNum.textContent = `${data.length}個項目件數`;
+    listNum.textContent = `${data.length}個待處理件數`;
 }
 
-//5.初始化"待處理"資料
-function renderTodoData(){
-    let str ='';
-    const todoData = data.filter(function(item){
-        return item.checked == "";
-    })
-    //console.log(todaData);
-
-    todoData.forEach(function(item,index){
-        str += `<li>
-        <label class="checkbox" for="">
-        <input type="checkbox" id="checkbox" data-num="${index}" ${item.checked}/>
-        <span>${item.content}</span>
-        </label>
-        <a href="#" class="delete" data-num="${index}"></a></li>`
-    })
-    //console.log(str);
-    list.innerHTML = str;
-    listNum.textContent = `${todoData.length}個待處理件數`;
+//3.tab切換
+const tab = document.getElementById('tab');
+let toggleStatus = 'all';
+tab.addEventListener('click',tabChange);
+function tabChange(e){    
+     toggleStatus = e.target.dataset.tab;
+     //console.log(toggleStatus);//確認有抓到值
+     let tabs = document.querySelectorAll('#tab li');
+    tabs.forEach(function(i){
+        i.classList.remove('active');
+    });
+    e.target.classList.add('active');
+    updateList();
 }
 
-//4.初始化"已完成"資料
-function renderFinshedData(){
-    let str = '';
-    const finshedData = data.filter(function(item){
-        return item.checked == "checked";
-    })
-    //console.log(finshedData);
-
-    finshedData.forEach(function(item,index){
-        str += `<li>
-        <label class="checkbox" for="">
-        <input type="checkbox" id="checkbox" data-num="${index}" ${item.checked}/>
-        <span>${item.content}</span>
-        </label>
-        <a href="#" class="delete" data-num="${index}"></a></li>`
-    })
-    //console.log(str);
-    list.innerHTML = str;
-    listNum.textContent = `${finshedData.length}個已完成件數`;
-}
-
-//初始化清除已完成項目
-// function renderCleanFinishedData(){
-//     let str = '';
-//     CleanData.forEach(function(item,index){
-//         str +=CleanData;
-//     })
-//     //console.log(str);
-//     list.innerHTML = str;
-//     listNum.textContent =`0個已完成件數`;    
-// }
-
-//3.點TAB選單切換全部、待處理、和已完成(搭配4.5.一起寫出)
-const tab = document.querySelector(".tab");
-tab.addEventListener('click',function(e){    
-    //console.log(e.target.getAttribute("class"));
-    if(e.target.getAttribute("class") ==='all active'){  
-        renderData();      
-        //console.log(e.target.getAttribute("class"));
-        return;
-    }   
-    else if(e.target.getAttribute("class") ==='todo'){
-        renderTodoData();  
-        return;
-    }
-    else if(e.target.getAttribute("class") ==='finished'){
-        //console.log(e.target.getAttribute("class"));
-        renderFinshedData();
+//4.刪除 & 切換 checked 狀態功能
+list.addEventListener('click',deleteAndChange);
+function deleteAndChange(e){
+    let Id = e.target.closest('li').dataset.id;
+    //console.log(Id);
+    if(e.target.classList.value =='delete' ){
+        //console.log(Id);
+        e.preventDefault();
+        data = data.filter(function(i){
+            return i.id != Id;
+        });
     }
     else{
-        return;
+        //切換checked狀態功能 
+        data.forEach(function(i,index){
+            if(i.id == Id){
+                if(data[index].checked == "checked"){
+                    data[index].checked ="";
+                }
+                else{
+                    data[index].checked= "checked";
+                }
+            }
+        });
     }
-})
+    //renderData();
+    updateList();
+}
 
-//2.判斷checkbox是否點擊到 true有點到寫入"checked"，  false沒點到寫入""
-list.addEventListener('click',function(e){
-    //console.log(e.target.checked);
-    let num = e.target.getAttribute("data-num")   
-    console.log(e.target.nodeName);
-    //console.log(num);      
-    if(e.target.checked === true){
-        console.log("有打勾");        
-        data[num].checked = "checked";
-        console.log(data[num]);              
+//5.更新待辦清單
+function updateList(){
+    let newData = [];
+    if(toggleStatus =='all'){
+        newData = data;
     }
-    else if(e.target.checked === false){
-        console.log("沒打勾");
-        data[num].checked = "";
-        console.log(data[num]);                
-    }    
+    else if(toggleStatus =='work'){
+        newData = data.filter(item => item.checked =="")
+    }
     else{
-        return;
-    } 
-})
-
-//6.新增事項功能
-btnAdd.addEventListener('click',function(e){
-    if(txtInput.value ==""){
-        alert("請輸入文字!");
-        return;
-    }    
-    let obj = {};
-    obj.content = txtInput.value;
-    obj.checked = "";
-    //console.log(obj);
-    data.push(obj);
-    renderData();
-})
-
-//7.刪除事項功能
-list.addEventListener('click',function(e){
-    //console.log("有點到");
-    if(e.target.nodeName ==="A"){
-        //console.log("有點到A連結");
-        let num = e.target.getAttribute("data-num");
-        console.log(num);
-        data.splice(num,1);
-        renderData();
+        newData = data.filter(item => item.checked == "checked")
     }
-})
+    let workNum = document.querySelector(".workNum")
+    let todoLength = data.filter(item =>  item.checked === "")
+    // 計算代辦事項
+    workNum.textContent = todoLength.length
+    
+    renderData(newData);
+    console.log(newData);
+}
 
-//8.清除已完成項目
-listCleanFinished.addEventListener('click',function(e){      
-    //console.log("有點到清除鍵");
-    const CleanData = data.filter(function(item){
-        return item.checked == "checked";
-    })
-    //console.log(CleanData);
-    CleanData.forEach(function(item){
-        data.splice(data.findIndex(function(el){
-            return el.checked == item.checked;
-        }),1)
-    })
-    renderData();
-})
+//renderData();
+updateList();
+
+//6.刪除已完成
+cleanDone.addEventListener('click',function(e){
+    e.preventDefault();
+    data = data.filter((i) => i.checked != 'checked');
+    updateList();
+});
 
 
-renderData();
+
